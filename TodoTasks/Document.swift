4db2cba2c14f5384bde9feae6043adb89f42e092
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2019-09-21 11:35:29 +0900

    init commit

---
+++ TodoTasks/Document.swift
@@ -0,0 +1,28 @@
+//
+// Document.swift
+// TodoTasks
+//
+// Copyright © 2019 MasaMatsu. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class var autosavesInPlace: Bool {
+        return true
+    }
+
+    override func makeWindowControllers() {
+        // Returns the Storyboard that contains your Document window.
+        let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+        let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Document Window Controller")) as! NSWindowController
+        self.addWindowController(windowController)
+    }
+
+}
---
+++ TodoTasks/ViewController.swift
@@ -0,0 +1,26 @@
+//
+// ViewController.swift
+// TodoTasks
+//
+// Copyright © 2019 MasaMatsu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
