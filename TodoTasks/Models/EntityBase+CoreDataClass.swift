a5926f2d0e7fd1889e1daa33f4cc587eb02d1f30
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2019-09-21 15:36:41 +0900

    EntityBase: Override willSave to set modified date parameter

--- TodoTasks/Models/EntityBase+CoreDataClass.swift
+++ TodoTasks/Models/EntityBase+CoreDataClass.swift
@@ -11,4 +11,19 @@ import CoreData
 @objc(EntityBase)
 public class EntityBase: NSManagedObject {
 
+    public override func willSave() {
+        super.willSave()
+
+        let now = Date()
+        if isInserted {
+            createdAt = now
+            updatedAt = now
+        }
+        else if isUpdated {
+            updatedAt = now
+            if isLogicalDeleted {
+                logicalDeletedAt = now
+            }
+        }
+    }
 }
