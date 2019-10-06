f3df04e3651d265b09360b2938e1c6b890696346
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2019-10-06 12:38:25 +0900

    Add SidebarViewController

---
+++ TodoTasks/Controllers/SidebarViewController.swift
@@ -0,0 +1,17 @@
+//
+// SidebarViewController.swift
+// TodoTasks
+//
+// Copyright © 2019 MasaMatsu. All rights reserved.
+//
+
+import Cocoa
+
+class SidebarViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+
+}
