6b7701ccfdf9a42f7337367354e2eb978a968e28
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2019-10-06 13:21:20 +0900

    SidebarViewController: Add tmpSaveTest

--- TodoTasks/Controllers/SidebarViewController.swift
+++ TodoTasks/Controllers/SidebarViewController.swift
@@ -15,5 +15,21 @@ class SidebarViewController: NSViewController {
     }
 
     @IBAction func ButtonToAddCategory(_ sender: Any) {
+        tmpSaveTest()
+    }
+
+    private func tmpSaveTest() {
+        let context = (representedObject as! Document).managedObjectContext!
+        let category = TodoCategory(context: context)
+        category.categoryName = "Test Category"
+        do {
+            try context.save()
+        }
+        catch {
+            let alert = NSAlert(error: error)
+            alert.alertStyle = .critical
+            alert.messageText = "save was faild."
+            alert.runModal()
+        }
     }
 }
