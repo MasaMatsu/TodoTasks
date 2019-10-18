bdd84cf936ed20fcb51d39b39fed2411b3c8dc3e
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2019-10-18 23:23:58 +0900

    Add child views

---
+++ SwiftUI-based/TodoTasks/TodoTasks/Views/SidebarView.swift
@@ -0,0 +1,20 @@
+//
+// SidebarView.swift
+// TodoTasks
+//
+// Copyright © 2019 MasaMatsu. All rights reserved.
+//
+
+import SwiftUI
+
+struct SidebarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SidebarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SidebarView()
+    }
+}
