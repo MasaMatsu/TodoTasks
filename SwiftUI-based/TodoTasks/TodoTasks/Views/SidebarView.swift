6a89ab058e908eed7d285ceb8e8d6da61e129436
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2019-10-18 23:29:49 +0900

    SidebarView: Add List

--- SwiftUI-based/TodoTasks/TodoTasks/Views/SidebarView.swift
+++ SwiftUI-based/TodoTasks/TodoTasks/Views/SidebarView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct SidebarView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+        List {
+            // TODO: Coding
+            NavigationLink(destination: DetailView()) {
+                Text("Category Name1")
+            }
+            NavigationLink(destination: DetailView()) {
+                Text("Category Name2")
+            }
+            NavigationLink(destination: DetailView()) {
+                Text("Category Name3")
+            }
+        }
     }
 }
 
