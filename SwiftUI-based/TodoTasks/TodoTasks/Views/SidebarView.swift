e5a76041825ee5f8224d7eed09a8cfd716a4571a
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2019-10-18 23:30:28 +0900

    SodebarView: Embed in VStack

--- SwiftUI-based/TodoTasks/TodoTasks/Views/SidebarView.swift
+++ SwiftUI-based/TodoTasks/TodoTasks/Views/SidebarView.swift
@@ -9,16 +9,18 @@ import SwiftUI
 
 struct SidebarView: View {
     var body: some View {
-        List {
-            // TODO: Coding
-            NavigationLink(destination: DetailView()) {
-                Text("Category Name1")
-            }
-            NavigationLink(destination: DetailView()) {
-                Text("Category Name2")
-            }
-            NavigationLink(destination: DetailView()) {
-                Text("Category Name3")
+        VStack(alignment: .leading) {
+            List {
+                // TODO: Coding
+                NavigationLink(destination: DetailView()) {
+                    Text("Category Name1")
+                }
+                NavigationLink(destination: DetailView()) {
+                    Text("Category Name2")
+                }
+                NavigationLink(destination: DetailView()) {
+                    Text("Category Name3")
+                }
             }
         }
     }
