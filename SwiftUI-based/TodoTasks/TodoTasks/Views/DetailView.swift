e796fe1310b80a1eabddb3b50abd86497b90c706
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2019-10-19 00:44:33 +0900

    DetailView: Add Buttons

--- SwiftUI-based/TodoTasks/TodoTasks/Views/DetailView.swift
+++ SwiftUI-based/TodoTasks/TodoTasks/Views/DetailView.swift
@@ -13,6 +13,19 @@ struct DetailView: View {
             // TODO: Coding
             Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
+
+            HStack {
+                Button(action: {
+                }) {
+                    Image(nsImage: NSImage(named: NSImage.addTemplateName)!)
+                }
+                Button(action: {
+                }) {
+                    Image(nsImage: NSImage(named: NSImage.removeTemplateName)!)
+                }
+                Spacer()
+            }
+            .padding([.leading, .bottom], 10)
         }
     }
 }
