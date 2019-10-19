dba6009bf4b814fbc10e1badc0ad0e86af7e2c09
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2019-10-19 19:58:45 +0900

    Add TaskRowView

---
+++ SwiftUI-based/TodoTasks/TodoTasks/Views/TaskRowView.swift
@@ -0,0 +1,20 @@
+//
+// TaskRowView.swift
+// TodoTasks
+//
+// Copyright © 2019 MasaMatsu. All rights reserved.
+//
+
+import SwiftUI
+
+struct TaskRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaskRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskRowView()
+    }
+}
