51374cf49e31bbf2851a2e2935d57d1091a9fb54
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2019-10-19 20:06:53 +0900

    TaskRowView: Add child views

--- SwiftUI-based/TodoTasks/TodoTasks/Views/TaskRowView.swift
+++ SwiftUI-based/TodoTasks/TodoTasks/Views/TaskRowView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct TaskRowView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            // TODO: Coding
+            VStack(alignment: .leading) {
+                Text("Task Name")
+                Text("2019/10/20 12:00")
+            }
+
+            Spacer()
+
+            Button(action: {
+            }) {
+                Image(nsImage: NSImage(named: NSImage.touchBarGetInfoTemplateName)!)
+            }
+            .buttonStyle(BorderlessButtonStyle())
+        }
     }
 }
 
