ff7e44a49de9d63848012c9724ee9f9babc08a7c
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2019-10-19 23:41:08 +0900

    Add TaskInfoView

---
+++ SwiftUI-based/TodoTasks/TodoTasks/Views/TaskInfoView.swift
@@ -0,0 +1,20 @@
+//
+// TaskInfoView.swift
+// TodoTasks
+//
+// Copyright © 2019 MasaMatsu. All rights reserved.
+//
+
+import SwiftUI
+
+struct TaskInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaskInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskInfoView()
+    }
+}
